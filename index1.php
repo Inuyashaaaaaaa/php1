38e34054be0ff4ea0f6fa2b819558ab33e1fcecd
Inuyashaaaaaaa <67748182+Inuyashaaaaaaa@users.noreply.github.com>
2024-10-02 19:17:39 +0800

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Discriminant</title>
+</head>
+<body>
+
+	 <!-- The form submits to the same page using GET method -->
+	<form action="testGet.php" method="GET">
+        <h1>Discriminant of a quadratic function</h1>
+		<!-- Input field for the numbers. -->
+		<p><input type="text" placeholder="A" name="A"></p>
+		<p><input type="text" placeholder="B" name="B"></p>
+        <p><input type="text" placeholder="C" name="C"></p>
+
+		<!-- Submit button -->
+		<p><input type="submit" value="Submit" name="getDiscriminant"></p>
+	</form>
+</body>
+</html>
